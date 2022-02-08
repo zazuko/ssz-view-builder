289424cd99f4dda523d542a1f0d1583315ec37bc
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-02-08 13:11:28 +0100

    have opener open zazuko converter

--- src/opener.js
+++ src/opener.js
@@ -1,18 +1,9 @@
 import { turtle } from '@tpluscode/rdf-string'
 
-let tab
-
 document.querySelector('button').addEventListener('click', () => {
   const form = document.querySelector('shaperone-form')
-  tab?.close()
 
-  tab = window.open(`about:blank`, 'shaperone')
-  const resourceTurtle = `<pre>${turtle`${form.resource.dataset}`.toString().replace(/</g, '&lt;')}</pre>`
-
-  tab.document.write(`
-    <head><title>Shaperone resource</title></head>
-    <iframe style="border: none; width: 100vw; height: 100vh;" 
-      src="data:text/html,${encodeURIComponent(resourceTurtle)}">
-    </iframe>
-  `)
+  const resourceTurtle = turtle`${form.resource.dataset}`.toString()
+  const converterUrl = `https://converter.zazuko.com/#value=${encodeURIComponent(resourceTurtle)}&format=text%2Fturtle`
+  window.open(converterUrl, 'shaperone')
 })
