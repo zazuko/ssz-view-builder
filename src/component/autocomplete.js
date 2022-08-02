c88f6dc1f33a09ff24fbc3aba4da3280f764900a
Tomasz Pluskiewicz <588128+undefined@users.noreply.github.com>
2022-08-02 09:41:03 +0000

    unused import

--- src/component/autocomplete.js
+++ src/component/autocomplete.js
@@ -7,7 +7,6 @@ import "@shoelace-style/shoelace/dist/components/menu/menu.js"
 import "@shoelace-style/shoelace/dist/components/menu-item/menu-item.js"
 import "@shoelace-style/shoelace/dist/components/tag/tag.js"
 import { getLocalizedLabel } from '@rdfjs-elements/lit-helpers'
-import { localizedLabel } from '@rdfjs-elements/lit-helpers/localizedLabel.js'
 import { renderItem } from './instancesSelect.js'
 import { rdfs } from '@tpluscode/rdf-ns-builders'
 
