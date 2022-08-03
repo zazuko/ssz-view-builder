aa39f59f71411b16e2f139c0dd0d6fb56524b416
Tomasz Pluskiewicz <588128+undefined@users.noreply.github.com>
2022-08-03 12:14:41 +0000

    improve the use of directives

--- src/component/autocomplete.js
+++ src/component/autocomplete.js
@@ -6,14 +6,15 @@ import "@shoelace-style/shoelace/dist/components/input/input.js"
 import "@shoelace-style/shoelace/dist/components/menu/menu.js"
 import "@shoelace-style/shoelace/dist/components/menu-item/menu-item.js"
 import "@shoelace-style/shoelace/dist/components/tag/tag.js"
-import { getLocalizedLabel } from '@rdfjs-elements/lit-helpers'
+import { localizedLabel } from '@rdfjs-elements/lit-helpers/localizedLabel.js'
 import { renderItem } from './instancesSelect.js'
-import { rdfs } from '@tpluscode/rdf-ns-builders'
+import { schema } from '@tpluscode/rdf-ns-builders'
 
 export function autocomplete(params, { update }) {
   const { value } = params
   const pointers = value.componentState.instances || []
   const freetextQuery = value.componentState.freetextQuery || ''
+  const selected = value.componentState.selected
 
   const search = (e) => {
     const [input] = e.composedPath()
@@ -26,17 +27,18 @@ export function autocomplete(params, { update }) {
     const selected = pointers.find(({ value }) => value === e.detail.item.value)
 
     params.updateComponentState({
-      freetextQuery: getLocalizedLabel(selected.out(rdfs.label)),
+      freetextQuery: '',
+      selected,
     })
     if (selected) {
       update(selected.term)
     }
   }
-
+debugger
   return html`
     <sl-dropdown @sl-hide=${stop} hoist>
       <sl-input slot="trigger" 
-                .value=${freetextQuery}
+                .value=${localizedLabel(selected, { property: schema.name, fallback: freetextQuery })}
                 @sl-input="${search}">
         <sl-icon name="search" slot="suffix"></sl-icon>
       </sl-input>
