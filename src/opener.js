cfe09df9cbce05991b9da5a0ed72e38c89818243
Tomasz Pluskiewicz <588128+undefined@users.noreply.github.com>
2022-08-15 06:55:11 +0000

    open yasgui on integ

--- src/opener.js
+++ src/opener.js
@@ -1,5 +1,6 @@
 import { turtle } from '@tpluscode/rdf-string'
 import { prepareViewPointer, createViewQuery } from './view.js'
+import { endpoint } from './queries/index.js'
 
 document.getElementById('converter-opener').addEventListener('click', () => {
   const form = document.querySelector('shaperone-form')
@@ -15,6 +16,13 @@ document.getElementById('yasgui-opener').addEventListener('click', () => {
   const view = prepareViewPointer(form.resource.dataset)
   const query = createViewQuery(view)
 
-  const converterUrl = `https://ld.stadt-zuerich.ch/sparql/#query=${encodeURIComponent(query)}&format=text%2Fturtle`
-  window.open(converterUrl, 'yasgiu')
+  const params = new URLSearchParams({
+    endpoint,
+    query,
+    format: 'text/turtle'
+  })
+
+  const converterUrl = new URL('https://ld.stadt-zuerich.ch/sparql/')
+  converterUrl.hash = params.toString()
+  window.open(converterUrl.toString(), 'yasgui')
 })
