35ab72fb7d52a93472e35bd18981d89313cc6897
Cristian <cristianvasquez@gmail.com>
2022-08-23 11:14:14 +0200

    I'm adding a button that opens the view in the cube-view

--- src/opener.js
+++ src/opener.js
@@ -29,3 +29,16 @@ document.getElementById('yasgui-opener').addEventListener('click', async () => {
   converterUrl.hash = params.toString()
   window.open(converterUrl.toString(), 'yasgui')
 })
+
+document.getElementById('cube-viewer-opener').addEventListener('click', async () => {
+  const { prepareViewPointer } = await import( './view.js')
+
+  const form = document.querySelector('shaperone-form')
+  const view = prepareViewPointer(form.resource.dataset)
+  const resourceTurtle = turtle`${view.dataset}`.toString()
+
+  const endpointUrl = encodeURIComponent('https://ld.stadt-zuerich.ch/query')
+  const cubeViewerUrl = `https://cubeviewerdemo.netlify.app/?endpointUrl=${endpointUrl}&view=${resourceTurtle}`
+
+  window.open(cubeViewerUrl, 'cubeViewer')
+})
