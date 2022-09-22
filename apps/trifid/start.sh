e43e08240ef250c5e1a32364647a4b0543b1c634
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-09-22 09:01:06 +0200

    trifid without reasoning

--- apps/trifid/start.sh
+++ apps/trifid/start.sh
@@ -4,4 +4,4 @@ set +x
 
 /app/server.js \
   --config=/config-sparql.json \
-  --sparql-endpoint-url="http://$SPARQL_USER:$SPARQL_PASSWORD@store:5820/view-builder/query?reasoning=true"
+  --sparql-endpoint-url="http://$SPARQL_USER:$SPARQL_PASSWORD@store:5820/view-builder/query"
