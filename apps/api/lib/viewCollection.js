68947a2f0797df4ccd4b9026d5985ff198f2bd84
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-10-13 13:29:44 +0200

    fix: perf problem with DESCRIBE

--- apps/api/lib/viewCollection.js
+++ apps/api/lib/viewCollection.js
@@ -1,5 +1,6 @@
-import { sparql } from '@tpluscode/sparql-builder'
+import { CONSTRUCT, sparql } from '@tpluscode/sparql-builder'
 import { schema, vcard } from '@tpluscode/rdf-ns-builders'
+import { VALUES } from '@tpluscode/sparql-builder/expressions'
 
 export function textFilter({ subject, object, variable }) {
   if (!object.value) {
@@ -23,4 +24,29 @@ export function textFilter({ subject, object, variable }) {
   
     FILTER( REGEX(str(${q}), "${object.value}", "i") )
   `
+}
+
+export function construct({ client }) {
+  return (members) => {
+    const resources = members.map(resource => ({ resource }))
+
+    // TODO: Construct generated from NodeShape
+    return CONSTRUCT`
+      ?resource ${schema.identifier} ?id ; 
+                ${schema.name} ?name ; 
+                ${schema.alternateName} ?alt .
+                
+      ?resource ${schema.author} ?author . ?author ${vcard.hasUID} ?authorName .
+    `.WHERE`
+      ${VALUES(...resources)}
+
+      ?resource ${schema.identifier} ?id ; 
+                ${schema.name} ?name ; 
+                ${schema.alternateName} ?alt .
+                
+      OPTIONAL {
+        ?resource ${schema.author} ?author . ?author ${vcard.hasUID} ?authorName .
+      }
+    `.execute(client.query)
+  }
 }
