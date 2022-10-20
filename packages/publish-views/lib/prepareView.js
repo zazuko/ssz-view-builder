66d6a6316926b78e7bbf41478109ca10a00f31eb
tpluscode <tpluscode@users.noreply.github.com>
2022-10-20 15:17:54 +0200

    feat: add void dataset links to views

--- packages/publish-views/lib/prepareView.js
+++ packages/publish-views/lib/prepareView.js
@@ -1,9 +1,14 @@
 import { prepareViewPointer } from '@view-builder/view-util'
+import { schema } from '@tpluscode/rdf-ns-builders'
 
 export default function (pointer) {
-  return prepareViewPointer(pointer, {
+  const view = prepareViewPointer(pointer, {
     removeEventListener: true,
     rename: true,
     removeLimitOffset: true,
   })
+
+  view.addIn(schema.dataset, view.namedNode(this.variables.get('well-known-views-dataset')))
+
+  return view
 }
