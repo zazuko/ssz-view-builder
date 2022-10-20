1580b40c638386ee50806401fb3a8942aea2d404
tpluscode <tpluscode@users.noreply.github.com>
2022-10-20 12:03:22 +0200

    feat: default limit, removed when publishing

--- packages/publish-views/lib/prepareView.js
+++ packages/publish-views/lib/prepareView.js
@@ -1,5 +1,9 @@
 import { prepareViewPointer } from '@view-builder/view-util'
 
 export default function (pointer) {
-  return prepareViewPointer(pointer, { removeEventListener: true, rename: true })
+  return prepareViewPointer(pointer, {
+    removeEventListener: true,
+    rename: true,
+    removeLimitOffset: true,
+  })
 }
