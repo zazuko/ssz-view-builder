dc79d79e23f32ed245360f2e613bdb4a6372b5e5
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-10-26 11:17:03 +0200

    feat: add dcat:Distribution to published views

--- packages/publish-views/lib/prepareView.js
+++ packages/publish-views/lib/prepareView.js
@@ -1,9 +1,27 @@
 import { prepareViewPointer } from '@view-builder/view-util'
+import { dcat, dcterms, rdf } from '@tpluscode/rdf-ns-builders'
 
 export default function (pointer) {
-  return prepareViewPointer(pointer, {
+  const view = prepareViewPointer(pointer, {
     removeEventListener: true,
     rename: true,
     removeLimitOffset: true,
   })
+
+  view.addOut(dcat.distribution, (rdfDistribution) => {
+    rdfDistribution
+      .addOut(rdf.type, dcat.Distribution)
+      .addOut(dcterms.format, 'RDF')
+      .addOut(dcat.mediaType, 'application/n-triples')
+      .addOut(dcat.downloadURL, view.namedNode(`${view.value}/observation/`))
+  })
+  view.addOut(dcat.distribution, (csvDistribution) => {
+    csvDistribution
+      .addOut(rdf.type, dcat.Distribution)
+      .addOut(dcterms.format, 'CSV')
+      .addOut(dcat.mediaType, 'text/csv')
+      .addOut(dcat.downloadURL, view.namedNode(`${view.value}/observation/?format=csv`))
+  })
+
+  return view
 }
