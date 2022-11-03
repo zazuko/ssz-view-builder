1b1abfaf8d6dfec021cf7d1a55ac39ed3c7974b2
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-11-03 14:34:02 +0100

    refactor: single way to find paths

--- packages/publish-views/lib/shapes.js
+++ packages/publish-views/lib/shapes.js
@@ -1,27 +1,25 @@
 import onetime from 'onetime'
 import { fromFile } from 'rdf-utils-fs'
 import $rdf from 'rdf-ext'
-import path from 'path'
-import { fileURLToPath } from 'url'
 import clownface from 'clownface'
 import { schema, sh } from '@tpluscode/rdf-ns-builders'
 import { createRequire } from 'module'
 import concat from 'barnard59-base/concat.js'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const shapePath = path.join(__dirname, '../shapes.ttl')
 const require = createRequire(import.meta.url)
+const metadataShapesPath = require.resolve('../shapes.ttl')
+const viewShapesPath = require.resolve('@view-builder/core/shape/ViewValidationShape.ttl')
 
 export const viewShape = onetime(async () => {
-  const dataset = await $rdf.dataset().import(fromFile(shapePath))
+  const dataset = await $rdf.dataset().import(fromFile(metadataShapesPath))
 
   return clownface({ dataset })
     .has(sh.targetClass, schema.Dataset)
 })
 
 export function validationShapes() {
-  const metadataShapes = fromFile(require.resolve('../shapes.ttl'))
-  const viewShapes = fromFile(require.resolve('@view-builder/core/shape/ViewValidationShape.ttl'))
+  const metadataShapes = fromFile(metadataShapesPath)
+  const viewShapes = fromFile(viewShapesPath)
 
   return concat.object(metadataShapes, viewShapes)
 }
