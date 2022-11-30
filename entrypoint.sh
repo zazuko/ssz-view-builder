3b2473740174c63e0886b7a859a32c2fa19c1d72
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-11-30 10:29:22 +0100

    feat: landing page improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -eu
 
+echo "Preparing environment"
+API_VERSION=$(cat apps/api/package.json | json version)
+UI_VERSION=$(cat apps/www/package.json | json version)
+
+export API_VERSION
+export UI_VERSION
+
 echo "Preparing environment resources"
 find apps/api/resources.live -name '*.ttl' -exec /bin/sh -c 'envsubst < $1 > $1.tmp && mv $1.tmp $1' -- {} \;
 
