f1a2b89d939bec5abe23410caa4923018914fe61
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-01-26 13:19:24 +0100

    fix: env

--- packages/core/env.js
+++ packages/core/env.js
@@ -1,8 +1,9 @@
-import { createEnv } from '@rdfine/env'
+import Environment from '@zazuko/env/Environment.js'
+import RdfineEnv from '@rdfine/env'
 import { ShFactory } from '@rdfine/shacl/Factory'
 import { HydraFactory } from '@rdfine/hydra/Factory'
 
-export default createEnv(
+export default new Environment([
   ShFactory,
   HydraFactory,
-)
+], { parent: RdfineEnv })
